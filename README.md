33f96d70ecb207573fe347b114c0605138b130a3
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-07-08 22:20:21 +0200

    update contacts and readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node-hw-01
+
+https://monosnap.com/file/6JDizXguNSiaLLoc8kKJlxsPbZIvje
+
+https://monosnap.com/file/gfvUhIZc7JtFMqepuWBnWdhcb9kDgO
+
+https://monosnap.com/file/c4H0skDiG1txSS5vQ4nXpH36lBKqaP
+
+https://monosnap.com/file/ZsJSFEoojZKym1yWQcx3C3CiuGwD9i
--- index.js
+++ index.js
@@ -18,25 +18,29 @@ program.parse(process.argv);
 const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      const allContacts = await listContacts();
-      return console.table(allContacts);
+  try {
+    switch (action) {
+      case "list":
+        const allContacts = await listContacts();
+        return console.table(allContacts);
 
-    case "get":
-      const contact = await getContactById(id);
-      return console.table(contact);
+      case "get":
+        const contact = await getContactById(id);
+        return console.table(contact);
 
-    case "add":
-      const addedContact = await addContact({ name, email, phone });
-      return console.table(addedContact);
+      case "add":
+        const addedContact = await addContact({ name, email, phone });
+        return console.table(addedContact);
 
-    case "remove":
-      const removedContact = await removeContact(id);
-      return console.table(removedContact);
+      case "remove":
+        const removedContact = await removeContact(id);
+        return console.table(removedContact);
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
 
