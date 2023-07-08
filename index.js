b36dc9633313702511114c9442a00b2306742a0d
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-07-08 22:10:27 +0200

    build: invoke action and commander options

--- index.js
+++ index.js
@@ -3,14 +3,41 @@ const {
   getContactById,
   removeContact,
   addContact,
-} = require("./contacts");
+} = require("./db");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-(async () => {
-  console.log(
-    await addContact({
-      name: "Simon Morton",
-      email: "dui.Fusce.diam@Donec.com",
-      phone: "(233) 738-2360",
-    })
-  );
-})();
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const contact = await getContactById(id);
+      return console.table(contact);
+
+    case "add":
+      const addedContact = await addContact({ name, email, phone });
+      return console.table(addedContact);
+
+    case "remove":
+      const removedContact = await removeContact(id);
+      return console.table(removedContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
